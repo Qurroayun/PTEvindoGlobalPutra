f0112cfe83d78b1742af9ba53b5207ff1b45c653
Qurroayun <kuriayun@gmail.com>
2025-06-24 10:47:47 +0700

    Update Redis service using ConfigService

--- src/common/redis/redis.service.spec.ts
+++ src/common/redis/redis.service.spec.ts
@@ -1,11 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RedisService } from './redis.service';
+import { ConfigModule } from '@nestjs/config';
+
+jest.mock('redis', () => ({
+  createClient: jest.fn(() => ({
+    connect: jest.fn().mockResolvedValue(undefined),
+    // ... mock lainnya
+  })),
+}));
 
 describe('RedisService', () => {
   let service: RedisService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test', // Gunakan file env khusus test
+        }),
+      ],
       providers: [RedisService],
     }).compile();
 
--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { createClient, RedisClientType } from 'redis';
+import {ConfigService} from '@nestjs/config';
 
 @Injectable()
 export class RedisService implements OnModuleInit{
     private client : RedisClientType;
- 
+
+    constructor(private configService: ConfigService) {}
+
     async onModuleInit() {
-        this.client = createClient({url : process.env.REDIS_URL})
+        this.client = createClient({url : this.configService.get('REDIS_URL')})
         await this.client.connect();
     }
 
